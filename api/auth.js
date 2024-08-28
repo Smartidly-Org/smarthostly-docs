3f85e94ce3694970397e91f796fa932c8a38679a
KumarBalajiNagaraj <balaji.volvocars@gmail.com>
2024-08-29 01:46:25 +0200

    Client check

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,21 @@
-// File: api/auth.js
-export default (req, res) => {
+// api/auth.js
+export default function handler(req, res) {
     const { query } = req;
     const redirectUri = `https://smarthostly-docs.vercel.app/admin`;
 
-    // Add your logic for handling authentication here
-    // This is a simplified example that does not handle the actual OAuth flow
-
-    res.writeHead(302, {
-        Location: redirectUri,
-    });
-    res.end();
-};
+    // Check if the request is authorized
+    if (query.access_token) {
+        // Redirect back to the admin with the access token
+        res.writeHead(302, {
+            Location: redirectUri,
+        });
+        res.end();
+    } else {
+        // If there is no access token, redirect to GitHub OAuth
+        const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=Ov23liikQ6h1wViR9G0M&scope=repo`;
+        res.writeHead(302, {
+            Location: githubAuthUrl,
+        });
+        res.end();
+    }
+}
