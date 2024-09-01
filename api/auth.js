bf72fbe4907a27a541d86a4e6c2a3b035b4f57a7
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2024-09-01 19:02:59 +0530

    IATW adds vercel.json and return res in auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,21 +1,37 @@
-// api/auth.js
-export default function handler(req, res) {
-    const { query } = req;
-    const redirectUri = `https://smarthostly-docs.vercel.app/admin`;
+import fetch from "node-fetch";
 
-    // Check if the request is authorized
-    if (query.access_token) {
-        // Redirect back to the admin with the access token
-        res.writeHead(302, {
-            Location: redirectUri,
-        });
-        res.end();
-    } else {
-        // If there is no access token, redirect to GitHub OAuth
-        const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=Ov23liikQ6h1wViR9G0M&scope=repo`;
-        res.writeHead(302, {
-            Location: githubAuthUrl,
-        });
-        res.end();
-    }
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=Ov23liikQ6h1wViR9G0M&scope=repo`;
+    res.writeHead(302, {
+      Location: githubAuthUrl,
+    });
+    res.end();
+    return;
+  }
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      client_id: "Ov23liikQ6h1wViR9G0M",
+      client_secret: "538783cf977d73285d5d7e57079802e9e8b52eb8",
+      code: code,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (data.access_token) {
+    return res.status(200).writeHead(302, {
+      Location: `https://smarthostly-docs.vercel.app/admin`,
+      "Set-Cookie": `token=${data.access_token}; Path=/; HttpOnly`,
+    });
+  } else {
+    res.status(500).json({ error: "Failed to obtain access token" });
+  }
 }
