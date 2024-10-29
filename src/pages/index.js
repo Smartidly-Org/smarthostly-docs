990edc22bf21888743a65d2788b3f33f11c03781
KumarBalajiNagaraj <balaji.volvocars@gmail.com>
2024-10-29 15:41:12 +0100

    IATW broken fix 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+            className="button button--secondary button--lg">
             Smarthostly Tutorial - Learn about anything in 5 minutes
           </Link>
         </div>
